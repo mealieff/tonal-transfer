0bf7cd1e98a5820c659b71da0ab56682e5c31ec8
Melissa Lieffers <mealieff@login1.hsn0.cm.bigred200.uits.iu.edu>
2025-03-25 12:59:10 -0400

    adding train.py

--- train.py
+++ train.py
@@ -1,3 +1,12 @@
-import simple-asr
+import numpy as np
+import random
+from IPython.display import Audio, display
+from transformers import AutoFeatureExtractor
+from huggingface_hub import notebook_login
 
+
+notebook_login()
+
+model_checkpoint = "facebook/wav2vec2-base"
+batch_size = 32
 
